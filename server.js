45e143292d524ea90f811b546671ddff37d48bd5
sepehr alipour <alipour.dev@gmail.com>
2022-05-31 14:36:14 -0400

    Update server.js
    add header information to server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Sepehr Alipour Student ID: 107296212 Date: 05/31/2022
+*
+*  Heroku Web App URL: https://mighty-tor-03182.herokuapp.com/
+*
+*  GitHub Repository URL: https://github.com/sepehr-alipour/web322-app
+*
+********************************************************************************/ 
 var express = require("express");
 var blog = require("./blog-service.js");
 
