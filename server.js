60275b9ab51b44d9e504e2572f317ae7acd34d82
Sepehr Alipour <alipour.dev@gmail.com>
2022-06-15 14:24:29 -0400

    added cloudinary,multer and streamifier and set configs on server.js file

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require("express");
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
 var blog = require("./blog-service.js");
+const streamifier = require('streamifier')
+
 
 var app = express();
 var PATH = require("path");
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-
+cloudinary.config({
+  cloud_name: 'SenecaWeb322',
+  api_key: '893155491545128',
+  api_secret: 'SFNgepI3K8TbYBzUj_U_NB7maq0',
+  secure: true  
+});
+const upload = multer();
 function onHttpStart() {
   console.log("Express http server listening on " + PORT);
 }
@@ -48,6 +58,9 @@ app.get("/categories", function (req, res) {
     .catch((rejectMessage) => {
       "message:" + rejectMessage;
     });
+});
+app.get("/posts/add", function (req, res) {
+  res.sendFile(PATH.join(__dirname, "/views/addPost.html"));
 });
 
 app.use((req, res) => {
