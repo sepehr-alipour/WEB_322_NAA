559032241273c3b84bb08013fc49d517ab6f18ef
Sepehr Alipour <alipour.dev@gmail.com>
2022-07-07 14:28:29 -0400

    get blog posts and show in blog page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+const stripJs = require("strip-js");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 var blog = require("./blog-service.js");
@@ -38,6 +38,9 @@ app.engine(
           return options.fn(this);
         }
       },
+      safeHTML: function (context) {
+        return stripJs(context);
+      },
     },
   })
 );
@@ -69,17 +72,98 @@ app.get("/about", function (req, res) {
     layout: false, // do not use the default Layout (main.hbs)
   });
 });
-app.get("/blog", function (req, res) {
-  blog
-    .getPublishedPosts()
-    .then((response) => {
-      res.json(response);
-    })
-    .catch((error) => {
-      res.send({ message: error });
-    });
-});
+app.get("/blog", async (req, res) => {
+  // Declare an object to store properties for the view
+  let viewData = {};
 
+  try {
+    // declare empty array to hold "post" objects
+    let posts = [];
+
+    // if there's a "category" query, filter the returned posts by category
+    if (req.query.category) {
+      // Obtain the published "posts" by category
+      posts = await blog.getPublishedPostsByCategory(req.query.category);
+    } else {
+      // Obtain the published "posts"
+      posts = await blog.getPublishedPosts();
+    }
+
+    // sort the published posts by postDate
+    posts.sort((a, b) => new Date(b.postDate) - new Date(a.postDate));
+
+    // get the latest post from the front of the list (element 0)
+    let post = posts[0];
+
+    // store the "posts" and "post" data in the viewData object (to be passed to the view)
+    viewData.posts = posts;
+    viewData.post = post;
+  } catch (err) {
+    viewData.message = "no results";
+  }
+
+  try {
+    // Obtain the full list of "categories"
+    let categories = await blog.getCategories();
+
+    // store the "categories" data in the viewData object (to be passed to the view)
+    viewData.categories = categories;
+  } catch (err) {
+    viewData.categoriesMessage = "no results";
+  }
+
+  // render the "blog" view with all of the data (viewData)
+  res.render("blog", { data: viewData });
+});
+app.get('/blog/:id', async (req, res) => {
+
+  // Declare an object to store properties for the view
+  let viewData = {};
+
+  try{
+
+      // declare empty array to hold "post" objects
+      let posts = [];
+
+      // if there's a "category" query, filter the returned posts by category
+      if(req.query.category){
+          // Obtain the published "posts" by category
+          posts = await blog.getPublishedPostsByCategory(req.query.category);
+      }else{
+          // Obtain the published "posts"
+          posts = await blog.getPublishedPosts();
+      }
+
+      // sort the published posts by postDate
+      posts.sort((a,b) => new Date(b.postDate) - new Date(a.postDate));
+
+      // store the "posts" and "post" data in the viewData object (to be passed to the view)
+      viewData.posts = posts;
+
+  }catch(err){
+      viewData.message = "no results";
+  }
+
+  try{
+      // Obtain the post by "id"
+      viewData.post = await blog.getPostById(req.params.id);
+  }catch(err){
+      viewData.message = "no results"; 
+  }
+
+  try{
+      // Obtain the full list of "categories"
+      let categories = await blog.getCategories();
+
+      // store the "categories" data in the viewData object (to be passed to the view)
+      viewData.categories = categories;
+  }catch(err){
+      viewData.categoriesMessage = "no results"
+  }
+
+  // render the "blog" view with all of the data (viewData)
+  res.render("blog", {data: viewData})
+});
 app.get("/posts", function (req, res) {
   if (req.query.category) {
     blog
@@ -89,7 +173,7 @@ app.get("/posts", function (req, res) {
         //res.json(response);
       })
       .catch((error) => {
-        res.render("posts", {message: error})
+        res.render("posts", { message: error });
         //res.send({ message: error });
       });
   } else if (req.query.minDate) {
@@ -101,9 +185,9 @@ app.get("/posts", function (req, res) {
         //  res.json(response);
       })
       .catch((error) => {
-        res.render("posts", {message: response})
+        res.render("posts", { message: response });
 
-      //  res.send({ message: error });
+        //  res.send({ message: error });
       });
   } else {
     blog
@@ -114,9 +198,9 @@ app.get("/posts", function (req, res) {
         //res.json(response);
       })
       .catch((error) => {
-        res.render("posts", {message: error})
+        res.render("posts", { message: error });
 
-      //  res.send({ message: error });
+        //  res.send({ message: error });
       });
   }
 });
@@ -128,7 +212,7 @@ app.get("/categories", function (req, res) {
       res.render("categories", { categories: response });
     })
     .catch((error) => {
-      res.render("categories", {message: error})
+      res.render("categories", { message: error });
     });
 });
 app.get("/posts/add", function (req, res) {
