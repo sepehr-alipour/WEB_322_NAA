3d1ddfaebb7e703fa3e58307da8662c9aef478a7
Sepehr Alipour <alipour.dev@gmail.com>
2022-07-07 13:43:02 -0400

    add posts.hbs for render posts informations

--- server.js
+++ server.js
@@ -13,7 +13,34 @@ var PATH = require("path");
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    helpers: {
+      navLink: function (url, options) {
+        return (
+          "<li" +
+          (url == app.locals.activeRoute ? ' class="active" ' : "") +
+          '><a href="' +
+          url +
+          '">' +
+          options.fn(this) +
+          "</a></li>"
+        );
+      },
+      equal: function (lvalue, rvalue, options) {
+        if (arguments.length < 3)
+          throw new Error("Handlebars Helper equal needs 2 parameters");
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
+        }
+      },
+    },
+  })
+);
 app.set("view engine", ".hbs");
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -25,6 +52,14 @@ const upload = multer();
 function onHttpStart() {
   console.log("Express http server listening on " + PORT);
 }
+
+app.use(function (req, res, next) {
+  let route = req.path.substring(1);
+  app.locals.activeRoute =
+    route == "/" ? "/" : "/" + route.replace(/\/(.*)/, "");
+  app.locals.viewingCategory = req.query.category;
+  next();
+});
 
 app.get("/", function (req, res) {
   res.redirect("/about");
@@ -50,28 +85,38 @@ app.get("/posts", function (req, res) {
     blog
       .getPostsByCategory(req.query.category)
       .then((response) => {
-        res.json(response);
+        res.render("posts", { posts: response });
+        //res.json(response);
       })
       .catch((error) => {
-        res.send({ message: error });
+        res.render("posts", {message: error})
+        //res.send({ message: error });
       });
   } else if (req.query.minDate) {
     blog
       .getPostsByMinDate(req.query.minDate)
       .then((response) => {
-        res.json(response);
+        res.render("posts", { posts: response });
+
+        //  res.json(response);
       })
       .catch((error) => {
-        res.send({ message: error });
+        res.render("posts", {message: response})
+
+      //  res.send({ message: error });
       });
   } else {
     blog
       .getAllPosts()
       .then((response) => {
-        res.json(response);
+        res.render("posts", { posts: response });
+
+        //res.json(response);
       })
       .catch((error) => {
-        res.send({ message: error });
+        res.render("posts", {message: error})
+
+      //  res.send({ message: error });
       });
   }
 });
