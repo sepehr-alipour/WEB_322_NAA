3ec0649a9577f8e1b64831f4d41818ac2360559c
sepehr alipour <alipour.dev@gmail.com>
2022-07-22 22:29:42 -0400

    Update server.js
    added header information to server.js file

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+*  WEB322 – Assignment 5
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Sepehr Alipour Student ID: 107296212 Date: 06/15/2022
+*
+*  Heroku Web App URL: https://tranquil-hamlet-98141.herokuapp.com
+*
+*  GitHub Repository URL: https://github.com/sepehr-alipour/web322-app
+*
+********************************************************************************/ 
+
 require("dotenv").config();
 
 var express = require("express");
