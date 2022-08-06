df2339924f884eca434a62d993f21a1f6bf02565
sepehr alipour <alipour.dev@gmail.com>
2022-08-05 23:40:08 -0400

    added header ,ready to submit assignment 6

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+/*********************************************************************************
+*  WEB322 – Assignment 6
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Sepehr Alipour Student ID: 107296212 Date: 08/05/2022
+*
+*  Heroku Web App URL: https://tranquil-hamlet-98141.herokuapp.com
+*
+*  GitHub Repository URL: https://github.com/sepehr-alipour/web322-app
+*
+********************************************************************************/ 
+
+
+
 require("dotenv").config();
 
 var express = require("express");
